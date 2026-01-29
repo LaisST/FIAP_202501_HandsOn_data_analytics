baeee16f18a46885b7c892efa7df83c8b4cb5d6e
Lais.ST <74365785+LaisST@users.noreply.github.com>
2026-01-28 22:49:00 -0300

    Add files via upload

---
+++ FIAP_B3/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import yfinance as yf
+
+st.set_page_config(
+    page_title="Painel da B3", 
+    layout="wide")
+
+st.header("**PAINEL DE PREÇO E DIVIDENDOS DA B3**")
+
+ticker = st.text_input("Digite o ticker da ação", "BBAS3")
+empresa = yf.Ticker(f"{ticker}.SA")
+
+ticketDF = empresa.history(
+    start="2014-01-01",
+    end="2025-12-31"
+)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(f"**Empresa:** {empresa.info['longName']}")
+with col2:
+    st.write(f"**Setor:** {empresa.info['industry']}")
+with col3:
+    st.write(f"**Preço Atual R$:** {empresa.info['currentPrice']:.2f}")
+
+st.line_chart(ticketDF['Close'])
+st.bar_chart(ticketDF['Dividends'])
